9fb5fdcef7330a25a907b6ea2578c92302e7d6fa
bwbowen15 <bwbowen15@gmail.com>
2025-09-08 19:29:58 -0400

    updated about and intro descriptions

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,10 +10,8 @@ function About(){
     return(
         <div className='about' id='about'>
             <div className='words'><h1>About Me</h1>
-            <p>Hi, I'm Brian, a Computer Science student at Penn State University with a love for programming, problem-solving, and learning new technologies. With two internships under my belt, I've gained hands-on experience
-                as a Software Engineer, and I am always looking for opportunites to learn and grow.
-                <br></br>
-                I love collaborating on innovative projects, whether its software development or something new. If you are interested in working together or just want to chat about tech, feel free to reach out - let's build something together!
+            <p>Hi, I’m Brian, a Software Engineer and senior studying computer science. I'm based in West Chester PA. I enjoy coding and building projects; feel free to reach out with opportunities or questions!
+                
             </p></div>
 
             <div className='logos'>
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -4,7 +4,7 @@ function Intro(){
 
     return(
         <div className='introText'>
-            <h1>Hi, I'm <span className='insideColor'>Brian Bowen</span>. I study <span className='insideColor'>Computer Science</span>  at Penn State University. Feel free to explore my website!</h1>
+            <h1>Hi, I'm <span className='insideColor'>Brian Bowen</span>. I'm a <span className='insideColor'>Computer Science</span> student at Penn State University and a <span className='insideColor'>Software Engineer</span> Intern at Erie Insurance.</h1>
         </div>
     )
 }
